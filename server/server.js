7adf4c8b4084a70043a1bfaa969c9c814f7cefdc
Seno Wijayanto <senowijayanto@gmail.com>
2017-09-03 09:14:36 +0700

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    return res.status(400).send();
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
@@ -48,12 +48,6 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
-  // findById
-    // success
-      // if todo - send it back
-      // if no todo - send back 404 with empty body
-    // error
-      // 400 - and send empty body back
 });
 
 app.listen(3000, () => {
